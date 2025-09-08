691f59ee5cec20d03fab482f917f520975e1cd34
Davi Okano <davi@okn.com.br>
2025-09-08 15:29:35 -0300

    adicionando migration CreateUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { Role } from '@roles/entities/Role'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1657974893356 } from './migrations/1657974893356-CreateRolesTable'
+import { CreateUsersTable1757355218066 } from './migrations/1757355218066-CreateUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1657974893356],
+  migrations: [CreateRolesTable1657974893356, CreateUsersTable1757355218066],
 })
