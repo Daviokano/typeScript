14e55270b592806cec4007f10c4ddc0c3e084917
Davi Okano <davi@okn.com.br>
2025-09-15 15:49:30 -0300

    adicionando o repository

--- src/users/repositories/IUsersRepository.ts
+++ src/users/repositories/IUsersRepository.ts
@@ -28,5 +28,6 @@ export interface IUsersRepository {
   findAll({ page, skip, take }): Promise<UsersPaginateProperties>
   findById(id: string): Promise<User | null>
   findByName(name: string): Promise<User | null>
+  findByEmail(email: string): Promise<User | null>
   delete(user: User): Promise<void>
 }
