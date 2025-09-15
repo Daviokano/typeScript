71efb4ed76ce7939bd7f609bf4514b406d3225ae
Davi Okano <davi@okn.com.br>
2025-09-15 14:39:33 -0300

    Create Relation users role

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -3,11 +3,12 @@ import { DataSource } from 'typeorm'
 import { CreateRolesTable1657974893356 } from './migrations/1657974893356-CreateRolesTable'
 import { CreateUsersTable1757355218066 } from './migrations/1757355218066-CreateUsersTable'
 import { AddRoleIdToUsersTable1757356292566 } from './migrations/1757356292566-AddRoleIdToUsersTable'
+import { User } from '@users/entities/User'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1657974893356,
     CreateUsersTable1757355218066,
