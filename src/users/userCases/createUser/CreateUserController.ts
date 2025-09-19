934300ceabb864ee410d63fa53a607ff9ed073a5
Davi Okano <davi@okn.com.br>
2025-09-19 15:48:03 -0300

    adicionando a controler do createUserCase

---
+++ src/users/userCases/createUser/CreateUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { CreateUserUseCase } from './createUserUseCase'
+import { container } from 'tsyringe'
+
+export class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const createUseCase = container.resolve(CreateUserUseCase)
+    const { name, email, password, isAdmin, roleId } = request.body
+    const user = await createUseCase.execute({
+      name,
+      email,
+      password,
+      isAdmin,
+      roleId,
+    })
+    return response.status(201).json(user)
+  }
+}
